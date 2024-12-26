ea820d4a631df071b1bc423c259979aa4994fa7e
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2024-12-26 13:50:08 +0000

    Aplicar cambios reconciliados

--- src/app/page.js
+++ src/app/page.js
@@ -6,16 +6,24 @@ import Miembros from "./components/Miembros";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>CLAN SICARIOS</h1>
-      <h3>L2 REBORN PROJECT</h3>
-      <h2>Nuestro propio lugar con juegos de azar y mujersuelas.</h2>
-      <div>
-        <Noticias/>
-        <Votaciones/>
-        <Info/>
-        <Miembros/>
+    <div className="min-h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 text-gray-300">
+      <header className="w-full py-4 flex flex-col sm:flex-row justify-around items-center bg-gray-800 shadow-lg">
+        <h1 className="text-4xl font-bold text-yellow-500 drop-shadow-lg">CLAN SICARIOS</h1>
+        <h3 className="text-xl sm:text-2xl italic text-yellow-400">L2 REBORN PROJECT</h3>
+        <h2 className="text-sm sm:text-lg mt-2 sm:mt-0 text-gray-400">
+          Nuestro propio lugar con <span className="text-yellow-500">juegos de azar</span> y{" "}
+          <span className="text-pink-500">mujersuelas</span>. 😎
+        </h2>
+      </header>
+      <main className="px-6 sm:px-12 py-8 text-center back">
+      <div className="bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 bg-opacity-20 border">
+        <h2 className="text-3xl font-semibold text-yellow-500 mb-4 drop-shadow-md opacity-90">Hubo una vez...</h2>
+        <p className="text-lg sm:text-xl leading-relaxed text-gray-400 opacity-80">
+          Un hombre tan aburrido como para hacer una web a su clan... ese hombre se llamaba{" "}
+          <span className="text-yellow-500 font-bold">JAKER</span>. Admirarlo y darle reco cuando se lo encuentren.
+        </p>
       </div>
+      </main>
     </div>
   );
 }
