3ca55ea9643f4bcb6cb353b1bf6566b1e5744876
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2025-02-25 23:55:20 +0000

    historias

--- src/app/historias/page.js
+++ src/app/historias/page.js
@@ -10,11 +10,8 @@ import { db } from "../firebase";
 export default function Historias() {
   const { user } = UserAuth();
   const [historia, setHistoria] = useState({
-    nombre: '',
-    lvl: '',
     motivo: '',
-    loQuePaso: '',
-    historia: ''
+    loQuePaso: ''
   });
 
   const [historias, setHistorias] = useState([]);
@@ -42,10 +39,10 @@ export default function Historias() {
   // Función para enviar la historia a Firebase
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (historia.nombre && historia.lvl && historia.motivo && historia.loQuePaso && historia.historia) {
+    if (historia.motivo && historia.loQuePaso) {
       try {
         await addHistoria(historia, user.uid);
-        setHistoria({ nombre: '', lvl: '', motivo: '', loQuePaso: '', historia: '' }); // Limpiar el formulario
+        setHistoria({ motivo: '', loQuePaso: '' }); // Limpiar el formulario
       } catch (error) {
         console.error("Error al guardar la historia:", error);
       }
@@ -66,7 +63,8 @@ export default function Historias() {
     <div className="min-h-screen bg-gray-900 text-white p-8 sm:p-20">
       <div className="max-w-3xl mx-auto text-center space-y-6">
         <h1 className="text-3xl sm:text-5xl font-bold text-red-500">🚨 BANEADOS INJUSTAMENTE 🚨</h1>
-        <h3 className="text-xl sm:text-2xl">¡Comparte tu historia!</h3>
+        <h3 className="text-xl sm:text-2xl">¡Comparte tu historia de forma anónima!</h3>
+        <p className="text-sm text-gray-400">Recuerda que no se permite el uso de malas palabras ni mencionar cosas fuera de lugar, como el nombre del servidor o el juego.</p>
       </div>
 
       {/* Contenedor de las columnas */}
@@ -74,28 +72,13 @@ export default function Historias() {
         {/* Formulario para contar la historia */}
         <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
           <form onSubmit={handleSubmit} className="space-y-4">
-            <input
-              type="text"
-              name="nombre"
-              placeholder="Tu nombre"
-              value={historia.nombre}
-              onChange={handleChange}
-              className="w-full p-3 bg-gray-700 rounded text-white placeholder-gray-400"
-            />
-            <input
-              type="text"
-              name="lvl"
-              placeholder="Nivel de tu personaje"
-              value={historia.lvl}
-              onChange={handleChange}
-              className="w-full p-3 bg-gray-700 rounded text-white placeholder-gray-400"
-            />
-            <input
-              type="text"
+            <textarea
               name="motivo"
               placeholder="Motivo del baneo"
               value={historia.motivo}
               onChange={handleChange}
+              rows="4"
+              maxLength="500"
               className="w-full p-3 bg-gray-700 rounded text-white placeholder-gray-400"
             />
             <textarea
@@ -103,15 +86,8 @@ export default function Historias() {
               placeholder="Lo que pasó realmente"
               value={historia.loQuePaso}
               onChange={handleChange}
-              rows="4"
-              className="w-full p-3 bg-gray-700 rounded text-white placeholder-gray-400"
-            />
-            <textarea
-              name="historia"
-              placeholder="Cuenta tu historia"
-              value={historia.historia}
-              onChange={handleChange}
               rows="6"
+              maxLength="500"
               className="w-full p-3 bg-gray-700 rounded text-white placeholder-gray-400"
             />
             <button
@@ -132,10 +108,8 @@ export default function Historias() {
             ) : (
               historias.map((hist) => (
                 <div key={hist.id} className="bg-gray-700 p-6 rounded-lg shadow-md">
-                  <h3 className="text-xl font-bold text-red-500">{hist.nombre} (LVL {hist.lvl})</h3>
                   <p className="text-gray-400">🚨 <strong>Motivo del baneo:</strong> {hist.motivo}</p>
                   <p className="text-gray-300 mt-2">📖 <strong>Lo que pasó:</strong> {hist.loQuePaso}</p>
-                  <p className="text-white mt-4">{hist.historia}</p>
                 </div>
               ))
             )}
