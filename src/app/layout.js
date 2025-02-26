6da31e0f6df395dacf6e323262db23f9fb183c60
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2025-02-26 01:48:08 +0000

    historias

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,25 +3,24 @@ import { AuthContextProvider } from "./context/AuthContext";
 import Header from "./components/header";
 
 export const metadata = {
-  title: "Baneados INJUSTAMENTE",
+  title: "BADBAN-L2",
   description: "LA VERDAD SOBRE LOS BANEOS DE REBORN",
 };
 
-export default function RootLayout({ children  }) {
-
+export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
       </head>
-      <body className='bg-gray-800 flex flex-col'>
+      <body className="bg-gray-800 flex flex-col">
         <AuthContextProvider>
-        <Header />
-                {children}
+          <Header />
+          <main>{children}</main>
         </AuthContextProvider>
       </body>
     </html>
   );
 }
-
-
