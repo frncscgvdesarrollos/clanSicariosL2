1e83e0d0c30c41abbcc090461c0e797dd16e7323
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2025-02-26 03:29:37 +0000

    historias

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Header from "./components/header";
 
 export const metadata = {
   title: "BADBAN-L2",
-  description: "LA VERDAD SOBRE LOS BANEOS DE REBORN",
+  description: "historias de baneos en el l2 ! ",
 };
 
 export default function RootLayout({ children }) {
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </head>
-      <body className="fondo flex flex-col">
+      <body className="bg-gray-900 flex flex-col">
         <AuthContextProvider>
           <Header />
           <main>{children}</main>
--- src/app/page.js
+++ src/app/page.js
@@ -36,15 +36,19 @@ export default function RootPage() {
   }
 
   return (
-    <div className="min-h-screen fondo text-white flex flex-col items-center justify-center space-y-6 p-4">
+    <div className="min-h-screen  fondo text-white flex flex-col items-center justify-center space-y-6 p-4">
+{/* Suggested code may be subject to a license. Learn more: ~LicenseLog:1853297041. */}
+      <div className="bg-gray-600 bg-opacity-80 rounded-lg flex flex-col items-center justify-center space-y-6 p-4">
       <span> la pagina esta en desarrollo </span>
       <h1 className="text-5xl font-bold text-center">BADBAN L2.</h1>
-      <span className="text-lg text-center">Recuerda que siempre pueden cometer errores los master del juego y que siempre debes tratar de hablar por las vias que te proporcionan, <br/> sin embargo muchas veces es injusto realmente.</span>
+      <span className="text-lg text-center color-gray-900">Recuerda que siempre pueden cometer errores los master del juego y que siempre debes tratar de hablar por las vias que te proporcionan, <br/> sin embargo muchas veces es injusto realmente.</span>
       <button 
         onClick={handleLogin} 
         className="mt-6 bg-blue-600 hover:bg-blue-700 p-4 rounded-lg text-white font-semibold text-xl w-full max-w-xs">
         Login con Google
       </button>
+
+      </div>
 
 
       
