23dca94c78cf3b6fbbb3af156534c0d451626116
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2025-02-26 00:35:28 +0000

    historias

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Asumimos que estás usando React Router para las rutas
+import Link from 'next/link'; // Importamos Link de Next.js
 
 const Header = () => {
   return (
     <header className="bg-gray-800 text-white p-4">
       <nav className="flex justify-between">
-        <Link to="/" className="text-xl font-bold">Inicio</Link>
+        <Link href="/" className="text-xl font-bold">Inicio</Link>
         <div className="space-x-4">
-          <Link to="/historias" className="hover:text-yellow-400">Historias</Link>
-          <Link to="/reglas" className="hover:text-yellow-400">Reglas</Link>
-          <Link to="/about" className="hover:text-yellow-400">About</Link>
+          <Link href="/historias" className="hover:text-yellow-400">Historias</Link>
+          <Link href="/reglas" className="hover:text-yellow-400">Reglas</Link>
+          <Link href="/about" className="hover:text-yellow-400">About</Link>
         </div>
       </nav>
     </header>
